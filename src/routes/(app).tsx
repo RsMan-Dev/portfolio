124a60ccedf64161189e926d4d3741620564bc9f
rsman <william.richer@unova.fr>
2025-03-26 21:31:58 +0100

    some changes

--- src/routes/(app).tsx
+++ src/routes/(app).tsx
@@ -1,13 +1,13 @@
 import {ParentComponent} from "solid-js";
 
-const AppLayout: ParentComponent = (props) => (
-  <>
+const AppLayout: ParentComponent = (props) => {
+  return <>
     <nav>
       <sticky-el class="rounded-2xl"><a href={"/home#whoami"}>About me</a></sticky-el>
       <sticky-el class="rounded-2xl"><a href={"/home#projects"}>My Projects</a></sticky-el>
     </nav>
     {props.children}
   </>
-)
+}
 
 export default AppLayout
